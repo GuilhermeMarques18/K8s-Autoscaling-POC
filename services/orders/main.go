f315a7da578c9d353eaffe4a6dfcf72d9ada66ef
Guilherme Marques <mr.gmrsouza@gmail.com>
2026-09-12 22:58:50 -0300

    feat: setup dual server initialization and makefile commands

--- services/orders/main.go
+++ services/orders/main.go
@@ -1,10 +1,17 @@
 package main
 
+import (
+	"log"
+
+	server "github.com/GuilhermeMarques18/K8s-Autoscaling-POC.git/services/orders/server"
+)
+
 func main() {
-	httpServer := NewHttpServer(":8081")
+	httpServer := server.NewHttpServer(":8081")
 	go httpServer.Run()
 
-	grpcServer := NewGRPCServer(":8080")
-	grpcServer.Run()
-
+	grpcServer := server.NewGRPCServer(":8080")
+	if err := grpcServer.Run(); err != nil {
+		log.Fatal(err)
+	}
 }
