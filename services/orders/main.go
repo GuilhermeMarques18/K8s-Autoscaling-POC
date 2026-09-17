ea49fb671a15894f4762c4b4e52214766724fb31
Guilherme Marques <mr.gmrsouza@gmail.com>
2026-09-17 00:33:30 -0300

    <mensagem curta em inglês>

--- services/orders/main.go
+++ services/orders/main.go
@@ -4,14 +4,25 @@ import (
 	"log"
 
 	server "github.com/GuilhermeMarques18/K8s-Autoscaling-POC.git/services/orders/server"
+	"go.uber.org/automaxprocs/maxprocs"
 )
 
 func main() {
+	undo, err := maxprocs.Set(maxprocs.Logger(log.Printf))
+	defer undo()
+	if err != nil {
+		log.Printf("Falha ao configura maxProcess: %v", err)
+	}
+
 	httpServer := server.NewHttpServer(":8081")
-	go httpServer.Run()
+	go func() {
+		if err := httpServer.Run(); err != nil {
+			log.Fatalf("HTTP server error: %v", err)
+		}
+	}()
 
 	grpcServer := server.NewGRPCServer(":8080")
 	if err := grpcServer.Run(); err != nil {
-		log.Fatal(err)
+		log.Fatalf("GRPC server error: %v", err)
 	}
 }
